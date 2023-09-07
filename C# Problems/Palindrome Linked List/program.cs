7fb4ae0170d4d4c75c0d6b2cc962d35aaadf38df
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-09-07 21:28:29 +0300

    Create program.cs

---
+++ C# Problems/Palindrome Linked List/program.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public bool IsPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode secondHalf = ReverseList(slow.next);
+        ListNode firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        
+        return true;
+    }
+    
+    private ListNode ReverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
