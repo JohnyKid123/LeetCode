907475ab89b2a6c47a6093f0e7e78c29620e7305
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-09-07 21:18:51 +0300

    Create program.cs

---
+++ C# Problems/Remove Nth Node From End of List/program.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy, fast = dummy;
+        
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        slow.next = slow.next.next;
+        
+        return dummy.next;
+    }
+}
