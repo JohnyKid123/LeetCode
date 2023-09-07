31f3f7dd85083cf96b0ee453ae342c1763acdea3
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-09-07 21:25:51 +0300

    Create program.cs

---
+++ C# Problems/Single Number/program.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int result = 0;
+        foreach (int num in nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
