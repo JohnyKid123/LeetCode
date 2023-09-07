23b21cfa55d634951525639542e162917dcf4dac
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-09-07 21:25:12 +0300

    Create program.cs

---
+++ C# Problems/Sqrt(x)/program.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int MySqrt(int x) 
+    {
+               double y = (double)x;
+        int z = (int)Math.Floor((Math.Sqrt(y)));
+        return z;    
+    }
+}
