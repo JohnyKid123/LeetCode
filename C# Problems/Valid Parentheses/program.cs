229c2d6c3fb285950b575f2dd2ce460f8998dc92
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-09-07 21:20:27 +0300

    Create program.cs

---
+++ C# Problems/Valid Parentheses/program.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsValid(string s) {
+    Stack<char> stack = new Stack<char>();
+    foreach(char c in s) {
+        if(c == '(' || c == '{' || c == '[') {
+            stack.Push(c);
+        } else if(stack.Count > 0 && 
+                  ((c == ')' && stack.Peek() == '(') ||
+                   (c == '}' && stack.Peek() == '{') ||
+                   (c == ']' && stack.Peek() == '['))) {
+            stack.Pop();
+        } else {
+            return false;
+        }
+    }
+    return stack.Count == 0;
+}
+
+}
